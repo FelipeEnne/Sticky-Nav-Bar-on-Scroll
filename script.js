734c14002ff5f8d6486513fde730057610cd957d
FelipeEnne <felipeenne@gmail.com>
2023-07-26 19:46:18 -0300

    add animated scroll to top

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const navList = document.querySelector(".nav-list");
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains("link")) {
+    navList.querySelector(".active").classList.remove("active");
+    navLink.classList.add("active");
+  }
+});
