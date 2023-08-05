827ba295428f2487d85eba58a8614582a09303a9
FelipeEnne <felipeenne@gmail.com>
2023-08-05 12:38:27 -0300

    add script to response menu

--- script.js
+++ script.js
@@ -51,3 +51,25 @@ function scrollToTop() {
     behavior: "smooth",
   });
 }
+
+// mobile menu
+
+// RESPONSIVE MOBILE MENU
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+const showMenu = () => {
+  hamburger.style.display = "none";
+  close.style.transform = "translateY(0)";
+  menu.style.transform = "translateY(0)";
+};
+
+const hideMenu = () => {
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menu.style.transform = "translateY(-200%)";
+};
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
